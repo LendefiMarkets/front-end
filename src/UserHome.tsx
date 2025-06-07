69eda0f2726350b4fa414993c59630e45ee70078
LendefiMarkets <alexei@lendefimarkets.com>
2025-06-06 22:13:11 -0700

    remove compilation errors

--- src/UserHome.tsx
+++ src/UserHome.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useAppKitAccount, useAppKitNetwork } from '@reown/appkit/react'
 import { Navigate } from 'react-router-dom'
 import CreateMarket from './components/CreateMarket'
